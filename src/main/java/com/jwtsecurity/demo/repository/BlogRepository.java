33e3ac51e443dd1a8405c4a9184bed88badab76e
llarios <lino.larios@gmail.com>
2019-12-29 18:22:55 -0600

    add repository

---
+++ src/main/java/com/jwtsecurity/demo/repository/BlogRepository.java
@@ -0,0 +1,16 @@
+package com.jwtsecurity.demo.repository;
+
+import com.jwtsecurity.demo.model.Blog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface BlogRepository extends JpaRepository<Blog, Integer> {
+
+    // custom query to search to blog post by title or content
+    List<Blog> findByTitleContainingOrContentContaining(String text, String textAgain);
+
+}
---
+++ src/main/java/com/jwtsecurity/demo/service/BlogService.java
@@ -0,0 +1,30 @@
+package com.jwtsecurity.demo.service;
+
+import com.jwtsecurity.demo.model.Blog;
+import com.jwtsecurity.demo.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public BlogService(final BlogRepository blogRepository) {
+        this.blogRepository = blogRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Blog> getAllBlogs(final int count) {
+        return this.blogRepository.findAll().stream().limit(count).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Blog> getBlog(final int id) {
+        return this.blogRepository.findById(id);
+    }
+}
