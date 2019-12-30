90fd60c07334785953b8d80a675bdcf66d685c83
llarios <lino.larios@gmail.com>
2019-12-29 18:23:45 -0600

    add user repo

---
+++ src/main/java/com/jwtsecurity/demo/repository/UserInfoRepository.java
@@ -0,0 +1,15 @@
+package com.jwtsecurity.demo.repository;
+
+import com.jwtsecurity.demo.model.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+
+    Boolean existsByUsername(String username);
+
+    UserInfo findByUsername(String username);
+
+
+}
